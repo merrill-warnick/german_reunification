ce2d36428ac37fe6219e0996e0f2e24cc95dbf05
Lea Bottmer <lbottmer@stanford.edu>
2020-08-16 21:45:30 -0700

    pictures

--- general_code.R
+++ general_code.R
@@ -146,11 +146,11 @@ data_synth <- readMat('germ_synth.mat')
 data_constr <- readMat('germ_constr_reg_nocov.mat')
 
 ### Treatment figure
-plot(1960:2003, data_did$Y.true, type = "l", lty = 2, ylim = c(0, 35000), xlim = c(1960,2003), col = "red", main = "West Germany: per capita GDP", xlab = "Year", ylab = "", las = 1, bty = 'L')
-lines(1960:2003, data_did$Y.did, lty = 1, col= "yellow")
-lines(1960:2003, data_elast$Y.elast, lty = 1, col= "purple4")
-lines(1960:2003, data_subset$Y.subs, lty = 1, col= "orange")
-lines(1960:2003, data_synth$Y.synth, lty = 1, col= "blue")
+plot(1960:2003, fit_synth$Y_true, type = "l", lty = 2, ylim = c(0, 35000), xlim = c(1960,2003), col = "red", main = "West Germany: per capita GDP", xlab = "Year", ylab = "", las = 1, bty = 'L')
+lines(1960:2003, fit_diff_in_diff$Y_est, lty = 1, col= "yellow")
+lines(1960:2003, fit_elastic_net$Y_est, lty = 1, col= "purple4")
+lines(1960:2003, fit_subs$Y_est, lty = 1, col= "orange")
+lines(1960:2003, fit_synth$Y_est, lty = 1, col= "blue")
 abline(v = 1989, col="black")
 abline(v = 1960, col = "grey96")
 abline(v = 1970, col = "grey96")
@@ -162,8 +162,8 @@ arrows(x0=1987, y0=32500,x1=1988, y1=32499, col=c("black"), lwd=1 , length = 0.0
 text(x=1981,y=32500,pos=4,label = "Reunification", cex = 0.5)
 
 ### Standard Errors
-tau <- cbind(data_did$Y.true[31:44]-data_synth$Y.synth[31:44],data_did$Y.true[31:44]-data_elast$Y.elast[31:44]) # cbind for each method
-std_err <- cbind(data_synth$std.err.synth.i, data_elast$std.err.elast.i)
+tau <- cbind(fit_diff_in_diff$Y_true[31:44]-fit_synth$Y_est[31:44],fit_diff_in_diff$Y_true[31:44]-fit_elastic_net$Y_est[31:44]) # cbind for each method
+std_err <- cbind(fit_synth$std_err_i, fit_elastic_net$std_err_i)
 
 plot(1990:2003, tau[,1], type = "l", lty = 1, ylim = c(-12500, 12500), xlim = c(1990,2003), col = "blue", main = "West Germany: Standard Errors", xlab = "Year", ylab = "", las = 1, bty = "L")
 lines(1990:2003, tau[,1]+1.96*std_err[,1], lty = 3, col= "blue")
@@ -203,12 +203,41 @@ legend("topright",legend=c("ADH synth. treatment","ADH treatment +/-1.96*std.err
 #legend("topright",legend=c("ADH synth. treatment","ADH treatment +/-1.96*std.err.",expression(paste("Elastic net treatment (opt. ", lambda,"and ",alpha,")" )),"Elastic net treatment +/-1.96*std.err."), col=c("blue","blue","plum2","plum2"),lty=c(1,2,1,2), ncol=1, bty = 'n', cex = 0.65)
 
 ## Weights
-weights <- cbind(data_synth$w.synth, data_elast$w.elast, data_subset$w.subs)
+weights <- cbind(fit_synth$w, fit_constr_reg$w, fit_elastic_net$w, fit_subs$w)
 theme_set(theme_bw())
 
 control_names <- c("USA", "GBR", "AUT", "BEL", "DNK", "FRA", "ITA", "NLD", "NOR", "CHE", "JPN", "GRC", "PRT", "ESP","AUS","NZL")
-weights_synth <- as.data.frame(cbind(control_names,weights[,3]))
+weights_synth <- as.data.frame(cbind(control_names,weights[,1]))
 colnames(weights_synth) <- c("controls","w")
 weights_synth$w <- as.numeric(as.character(weights_synth$w))
 p <- ggplot(weights_synth, aes(x=controls, y=w))+geom_bar(stat="identity", fill = "blue",color ="black", show.legend = FALSE)+labs(title="",x="", y = "Original synth.")+scale_fill_manual(values = c("royalblue"))+ylim(-1, 1)+coord_flip()
+
+
+weights_synth <- as.data.frame(cbind(control_names,weights[,2]))
+colnames(weights_synth) <- c("controls","w")
+weights_synth$w <- as.numeric(as.character(weights_synth$w))
+p1 <- ggplot(weights_synth, aes(x=controls, y=w))+geom_bar(stat="identity", fill = "green",color ="black", show.legend = FALSE)+labs(title="",x="", y = "Reg./w.restr.")+scale_fill_manual(values = c("green"))+ylim(-1, 1)+coord_flip()
+
+weights_synth <- as.data.frame(cbind(control_names,weights[,3]))
+colnames(weights_synth) <- c("controls","w")
+weights_synth$w <- as.numeric(as.character(weights_synth$w))
+p2 <- ggplot(weights_synth, aes(x=controls, y=w))+geom_bar(stat="identity", fill = "purple",color ="black", show.legend = FALSE)+labs(title="",x="", y = "Elastic Net")+scale_fill_manual(values = c("purple"))+ylim(-1, 1)+coord_flip()
+
+weights_synth <- as.data.frame(cbind(control_names,weights[,4]))
+colnames(weights_synth) <- c("controls","w")
+weights_synth$w <- as.numeric(as.character(weights_synth$w))
+p3 <- ggplot(weights_synth, aes(x=controls, y=w))+geom_bar(stat="identity", fill = "orange",color ="black", show.legend = FALSE)+labs(title="",x="", y = "Best subset")+scale_fill_manual(values = c("orange"))+ylim(-1, 1)+coord_flip()
+
+
+
+par(mfrow=c(1,4))
 p
+p1
+p2
+p3
+layout(matrix(c(1,2,3,4), 1, 4, byrow = TRUE))
+
+library(egg)
+figure <- ggarrange(p, p1, p2,p3,
+                    ncol = 4, nrow = 1)
+figure
