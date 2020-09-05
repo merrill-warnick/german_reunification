227acc08c062f120496fadbbc4506b745230ccd7
Lea Bottmer <lbottmer@stanford.edu>
2020-09-05 13:55:27 -0700

    Change plotting code

--- general_code_germ.R
+++ general_code_germ.R
@@ -111,7 +111,7 @@ Y <- cbind(Y1, Y0)
 # [Z1,Z0]
 Z <- cbind(Z1, Z0)
   
-output <- list( "X" = X, "Y" = Y, "Z" = Z)
+data <- list( "X" = X, "Y" = Y, "Z" = Z)
 
 
 #"spec" and "years" are the parameters that are different here.
@@ -164,9 +164,7 @@ fit_diff_in_diff <- general_estimate(data$Y, data$Z, data$X, W, method = "diff_i
 # Save matrices for future reference and plots
 
 # Elastic Net 
-#save(list = c("w", "int", "Y_est", "Y_true", 
-#              "std_err_i", "std_err_t", "std_err_it"), 
-#     file = "germ_elast_nocov.RData")
+save(fit_elastic_net, file = "germ_elast_nocov.RData")
 writeMat("germ_elast_nocov.mat", 
          w = fit_elastic_net$w, int = fit_elastic_net$int, 
          Y_est = fit_elastic_net$Y_est, Y_true = fit_elastic_net$Y_true, 
@@ -174,21 +172,8 @@ writeMat("germ_elast_nocov.mat",
          std_err_t = fit_elastic_net$std_err_t, 
          std_err_it = fit_elastic_net$std_err_it)
 
-# Best subset 
-#save(list = c("w", "int", "Y_est", "Y_true", 
-#              "std_err_i", "std_err_t", "std_err_it"), 
-#     file = "germ_subs_nocov.RData")
-writeMat("germ_subs_nocov.mat", 
-         w = fit_subs$w, int = fit_subs$int, 
-         Y_est = fit_subs$Y_est, Y_true = fit_subs$Y_true, 
-         std_err_i = fit_subs$std_err_i, 
-         std_err_t = fit_subs$std_err_t, 
-         std_err_it = fit_subs$std_err_it)
-
 # Constrained regression
-#save(list = c("w", "int", "Y_est", "Y_true", 
-#              "std_err_i", "std_err_t", "std_err_it"), 
-#     file = "germ_constr_reg_nocov.RData")
+save(fit_constr_reg, file = "germ_constr_reg_nocov.RData")
 writeMat("germ_constr_reg_nocov.mat", 
          w = fit_constr_reg$w, int = fit_constr_reg$int, 
          Y_est = fit_constr_reg$Y_est, Y_true = fit_constr_reg$Y_true, 
@@ -196,36 +181,57 @@ writeMat("germ_constr_reg_nocov.mat",
          std_err_t = fit_constr_reg$std_err_t, 
          std_err_it = fit_constr_reg$std_err_it)
 
+# Best subset 
+save(fit_subs, file = "germ_subs_nocov.RData")
+writeMat("germ_subs_nocov.mat", 
+         w = fit_subs$w, int = fit_subs$int, 
+         Y_est = fit_subs$Y_est, Y_true = fit_subs$Y_true, 
+         std_err_i = fit_subs$std_err_i, 
+         std_err_t = fit_subs$std_err_t, 
+         std_err_it = fit_subs$std_err_it)
+
+# Synthetic Control
+save(fit_synth, file = "germ_synth_nocov.RData")
+writeMat("germ_synth_nocov.mat", 
+         w = fit_synth$w, int = fit_synth$int, 
+         Y_est = fit_synth$Y_est, Y_true = fit_synth$Y_true, 
+         std_err_i = fit_synth$std_err_i, 
+         std_err_t = fit_synth$std_err_t, 
+         std_err_it = fit_synth$std_err_it)
+
+# Diff-in-diff
+save(fit_diff_in_diff, file = "germ_did_nocov.RData")
+writeMat("germ_did_nocov.mat", 
+         w = fit_diff_in_diff$w, int = fit_diff_in_diff$int, 
+         Y_est = fit_diff_in_diff$Y_est, Y_true = fit_diff_in_diff$Y_true, 
+         std_err_i = fit_diff_in_diff$std_err_i, 
+         std_err_t = fit_diff_in_diff$std_err_t, 
+         std_err_it = fit_diff_in_diff$std_err_it)
+
 ###########################
 ########## Plots ##########
 ###########################
 
-## Load data -> or can also simply call from before
-data_did <- readMat('germ_did_nocov.mat')
-data_elast <- readMat('germ_elast_nocov.mat')
-data_subset <- readMat('germ_subs_nocov.mat')
-data_synth <- readMat('germ_synth.mat')
-data_constr <- readMat('germ_constr_reg_nocov.mat')
-
 ### Treatment figure
-plot(1960:2003, data_did$Y.true, type = "l", lty = 2, ylim = c(0, 35000), xlim = c(1960,2003), col = "red", main = "West Germany: per capita GDP", xlab = "Year", ylab = "", las = 1, bty = 'L')
-lines(1960:2003, data_did$Y.did, lty = 1, col= "yellow")
-lines(1960:2003, data_elast$Y.elast, lty = 1, col= "purple4")
-lines(1960:2003, data_subset$Y.subs, lty = 1, col= "orange")
-lines(1960:2003, data_synth$Y.synth, lty = 1, col= "blue")
+plot(1960:2003, fit_diff_in_diff$Y_true, type = "l", lty = 2, ylim = c(0, 35000), xlim = c(1960,2003), col = "red", main = "West Germany: per capita GDP", xlab = "Year", ylab = "", las = 1, bty = 'L')
+lines(1960:2003, fit_synth$Y_est, lty = 1, col= "blue")
+lines(1960:2003, fit_constr_reg$Y_est, lty = 1, col= "green")
+lines(1960:2003, fit_elastic_net$Y_est, lty = 1, col= "purple4")
+lines(1960:2003, fit_subs$Y_est, lty = 1, col= "orange")
+lines(1960:2003, fit_diff_in_diff$Y_est, lty = 1, col= "yellow")
 abline(v = 1989, col="black")
 abline(v = 1960, col = "grey96")
 abline(v = 1970, col = "grey96")
 abline(v = 1980, col = "grey96")
 abline(v = 1990, col = "grey96")
 abline(v = 2000, col = "grey96")
-legend("topleft",legend=c("Actual data","Difference-in-Differences", expression(paste("Elastic net (opt. ", lambda," and ",alpha,")" )),"Best subset (opt. k)", "Original synth."), col=c("red","yellow","purple4","orange","blue"),lty=c(2,1,1,1,1), ncol=1, bty = 'n', cex = 0.7)
+legend("topleft",legend=c("Actual data", "Original synth.", "Regression w/restrictions",expression(paste("Elastic net (opt. ", lambda," and ",alpha,")" )),"Best subset (opt. k)","Difference-in-Differences"), col=c("red","blue","green","purple4","orange","yellow"),lty=c(2,1,1,1,1), ncol=1, bty = 'n', cex = 0.7)
 arrows(x0=1987, y0=32500,x1=1988, y1=32499, col=c("black"), lwd=1 , length = 0.05,xpd=TRUE)
 text(x=1981,y=32500,pos=4,label = "Reunification", cex = 0.5)
 
 ### Standard Errors
-tau <- cbind(data_did$Y.true[31:44]-data_synth$Y.synth[31:44],data_did$Y.true[31:44]-data_elast$Y.elast[31:44]) # cbind for each method
-std_err <- cbind(data_synth$std.err.synth.i, data_elast$std.err.elast.i)
+tau <- cbind(fit_synth$Y_true[31:44]-fit_synth$Y_est[31:44],fit_elastic_net$Y_true[31:44]-fit_elastic_net$Y_est[31:44]) # cbind for each method
+std_err <- cbind(fit_synth$std_err_i, fit_elastic_net$std_err_i)
 
 plot(1990:2003, tau[,1], type = "l", lty = 1, ylim = c(-12500, 12500), xlim = c(1990,2003), col = "blue", main = "West Germany: Standard Errors", xlab = "Year", ylab = "", las = 1, bty = "L")
 lines(1990:2003, tau[,1]+1.96*std_err[,1], lty = 3, col= "blue")
@@ -243,28 +249,7 @@ abline(v = 2000, col = "grey96")
 abline(v = 2002, col = "grey96")
 legend("topright",legend=c("ADH synth. treatment","ADH treatment +/-1.96*std.err.",expression(paste("Elastic net treatment (opt. ", lambda,"and ",alpha,")" )),"Elastic net treatment +/-1.96*std.err."), col=c("blue","blue","plum2","plum2"),lty=c(1,2,1,2), ncol=1, bty = 'n', cex = 0.65)
 
-## Counterfactual
-#tau_co <- cbind(data_elast$Y.true.co[(data_elast$T0.co+1):(data_elast$T0.co+data_elast$T1.co)]-data_synth$Y.synth.co[(data_elast$T0.co+1):(data_elast$T0.co+data_elast$T1.co)],data_synth$Y.true.co[(data_elast$T0.co+1):(data_elast$T0.co+data_elast$T1.co)]-data_elast$Y.elast.co[(data_elast$T0.co+1):(data_elast$T0.co+data_elast$T1.co)]) # cbind for each method
-#std_err_co <- cbind(data_synth$std.err.synth.i.co, data_elast$std.err.elast.i.co)
-
-#plot(1981:1989, tau_co[,1], type = "l", lty = 1, ylim = c(-12500, 12500), xlim = c(1981,1989), col = "blue", main = "West Germany: Counterfactual", xlab = "Year", ylab = "", las = 1, bty = "L")
-#lines(1981:1989, tau_co[,1]+1.96*std_err_co[,1], lty = 3, col= "blue")
-#lines(1981:1989, tau_co[,1]-1.96*std_err_co[,1], lty = 3, col= "blue")
-#lines(1981:1989, tau_co[,2], lty = 1, col= "plum2")
-#lines(1981:1989, tau_co[,2]+1.96*std_err_co[,2], lty = 2, col= "plum2")
-#lines(1981:1989, tau_co[,2]-1.96*std_err_co[,2], lty = 2, col= "plum2")
-#abline(h = 0, col= "black")
-#abline(v = 1982, col = "grey96")
-#abline(v = 1983, col = "grey96")
-#abline(v = 1984, col = "grey96")
-#abline(v = 1985, col = "grey96")
-#abline(v = 1986, col = "grey96")
-#abline(v = 1987, col = "grey96")
-#abline(v = 1988, col = "grey96")
-#abline(v = 1989, col = "grey96")
-#legend("topright",legend=c("ADH synth. treatment","ADH treatment +/-1.96*std.err.",expression(paste("Elastic net treatment (opt. ", lambda,"and ",alpha,")" )),"Elastic net treatment +/-1.96*std.err."), col=c("blue","blue","plum2","plum2"),lty=c(1,2,1,2), ncol=1, bty = 'n', cex = 0.65)
-
-## Weights
+# Weights
 weights <- cbind(data_synth$w.synth, data_elast$w.elast, data_subset$w.subs)
 theme_set(theme_bw())
 
